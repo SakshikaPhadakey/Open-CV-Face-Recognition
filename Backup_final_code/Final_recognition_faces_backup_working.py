53ec3a7c89aa0f90c9bc8218dcb30ec2bc883174
SakshikaPhadakey <saakshikaphadakey@gmail.com>
2021-12-19 11:22:43 +0530

    FaceRecognition-modification_version3

--- Backup_final_code/Final_recognition_faces_backup_working.py
+++ Backup_final_code/Final_recognition_faces_backup_working.py
@@ -13,10 +13,6 @@ eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
 
 speaker = pyttsx3.init()
-
-text = f"Welcome Fight Club Team , Beginning the scan"
-speaker.say(text)
-speaker.runAndWait()
 r = sr.Recognizer()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -25,7 +21,6 @@ recognizer.read("./recognizers/face-trainner.yml")
 
 #get labels from the dictionary using pickles
 labels = {"person_name": 1}
-detected_face= []
 
 with open("pickles/face-labels.pickle", 'rb') as f:
 	og_labels = pickle.load(f)
@@ -43,51 +38,50 @@ while(True):
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
 	for (x, y, w, h) in faces:
-		# Create rectangle around the face
-		cv2.rectangle(frame, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 4)
 
-		# Recognize the face belongs to which ID
-		Id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 		roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
 		roi_color = frame[y:y+h, x:x+w]
-		# # recognize? deep learned model predict keras tensorflow pytorch scikit learn
+		# recognize? deep learned model predict keras tensorflow pytorch scikit learn
 		id_, conf = recognizer.predict(roi_gray)
-		if confidence>=54 and confidence <= 85:
+		if conf>=54 and conf <= 85:
 			font = cv2.FONT_HERSHEY_SIMPLEX
 			name = labels[id_]
 			color = (255, 255, 255)
 			stroke = 2
 			num = int(num) + 1
-			#cv2.putText(frame, name+', ID:'+str(num),(x,y), font, 1, color, stroke, cv2.LINE_AA)
+			cv2.putText(frame, name+', ID:'+str(num),(x,y), font, 1, color, stroke, cv2.LINE_AA)
 
-		cv2.rectangle(frame, (x - 22, y - 90), (x + w + 22, y - 22), (0, 255, 0), -1)
-		cv2.putText(frame, name + 'ID:' +str(num), (x, y - 40), font, 1, (255, 255, 255), 3)
-		detected_face.append(name)
+		if num > 1:
+			text1 = "Multiple Faces Detected, Please Speak the youtuber name you want to scan"
+			speaker.say(text1)
+			speaker.runAndWait()
+			with sr.Microphone() as source:
+				#r.pause_threshold = 1
+				audio = r.listen(source)
+				query = r.recognize_google(audio, language='en-in')
+				print(query)
 
-	cv2.putText(frame, 'Number of faces: ' + str(len(faces)), (40, 40), font, 1, (255, 0, 0), 2)
-	cv2.imshow('frame',frame)
-	cv2.waitKey(3)
-
-	if num > 1:
-		text1 = "Multiple Faces Detected, Please Speak the youtuber name you want to scan"
-		speaker.say(text1)
-		speaker.runAndWait()
-		with sr.Microphone() as source:
-			audio = r.listen(source)
-			try:
-				get = r.recognize_google(audio)
-				speaker.say(name)
+				speaker.say(query)
 				speaker.runAndWait()
 
-			except sr.UnknownValueError:
-				print('error')
-			except sr.RequestError as e:
-				print('failed'.format(e))
-		break
+		img_item = "test.png"
+		cv2.imwrite(img_item, roi_color)
+
+		color = (0, 255, 0) #BGR 0-255
+		stroke = 2
+		end_cord_x = x + w
+		end_cord_y = y + h
+		cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+	cv2.putText(frame, 'Number of faces: ' + str(len(faces)), (40, 40), font, 1, (255, 0, 0), 2)
+
+	cv2.imshow('frame',frame)
+
+
 	if cv2.waitKey(20) & 0xFF == ord('q'):
 		break
 
-if name in set(detected_face):
+if name:
 	text = f"Welcome {name} , Your are authorized"
 	speaker.say(text)
 else:
